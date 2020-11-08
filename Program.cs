4a3088e036ffdaf255e1a9db726eb1ed3c6104b9
Gessielle <gessielle-barossi@hotmail.com>
2020-11-08 20:07:13 -0300

    Solicite a idade do usuário. Avalie se ele pode ser doador de sangue

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Doador_Sangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade; 
+
+            Console.WriteLine("Digite sua idade: ");
+            idade  =  int.Parse(Console.ReadLine());
+
+            if(idade <= 17)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Você não pode ser doador de sangue ");
+                Console.WriteLine("Pressione ENTER para sair! ");
+                Console.ReadKey();
+            }
+            if (idade >= 18 && idade <= 67)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Você pode ser doador de sangue");
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+            if (idade >= 68)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Você não pode ser doador de sangue");
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+        }
+    }
+} 
